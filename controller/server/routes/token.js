aa8f24571339928ad0ca727f56b441e3a5f7cdf7
Ekpang Michael <weezyval93@gmail.com>
2019-11-21 14:26:44 +0100

    Update token.js

--- controller/server/routes/token.js
+++ controller/server/routes/token.js
@@ -3,6 +3,6 @@ import Message from '../controllers/Message';
 
 const tokenRouter = Router();
 
-tokenRouter.post('/token/', Message.getToken);
+tokenRouter.post('/token', Message.getToken);
 
 export default tokenRouter;
